248a65c0e8750c21775d7b60c215c7c3639858ae
RAMYA-YANDRAPATI <143236157+RAMYA-YANDRAPATI@users.noreply.github.com>
2023-09-11 12:03:01 +0530

    Add files via upload

---
+++ task3.js
@@ -0,0 +1,36 @@
+
+            const inputbox=document.getElementById("search");
+            const list=document.getElementById("list");
+            function addtask(){
+                if(inputbox.value===''){
+                    alert("you must write something");
+                }
+                else{
+                    let li=document.createElement("li");
+                    li.innerHTML=inputbox.value;
+                    list.appendChild(li);
+                    let span=document.createElement("span");
+                    span.innerHTML="\u00d7";
+                    li.appendChild(span)
+                }
+                inputbox.value="";
+                saveData();
+            }
+        list.addEventListener("click",function(e)
+        {
+            if(e.target.tagName==="LI"){
+                e.target.classList.toggle("checked");
+                saveData();
+            }
+            else if(e.target.tagName==="SPAN"){
+                e.target.parentElement.remove();
+                saveData();
+            }
+        },false);
+        function saveData(){
+            localStorage.setItem("data",list.innerHTML);
+        }
+        function showtask(){
+            list.innerHTML=localStorage.getItem("data");
+        }
+        showtask();
